1d0ea2fdd88865c18311731c2728bfb00b0969f3
charcolle <charcolle.1234@gmail.com>
2018-04-26 00:10:44 +0900

    develop : Timeline script template creator

---
+++ Assets/Plugins/TimelineScriptTemplate/Editor/Script/TimelineScriptTemplateCreator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+namespace charcolle.Utility.TimelineScriptTemplate {
+
+    public class TimelineScriptTemplateCreator : MonoBehaviour {
+
+        private readonly static string PANEL_TITLE       = "Create Timeline Asset Script";
+        private readonly static string PANEL_DEFAULTNAME = "NewTimelineScript";
+
+        [MenuItem( "Assets/Create/Timeline Script", menuItem = "Assets/Create/Timeline Script", priority = 460 )]
+        private static void Create() {
+
+            var savePath = EditorUtility.SaveFilePanel( PANEL_TITLE, FileHelper.GetSelectAssetPath(), PANEL_DEFAULTNAME, "" );
+            if( !string.IsNullOrEmpty( savePath ) ) {
+                onCreate( FileHelper.SystemPathToAssetPath( savePath ) );
+            }
+
+        }
+
+        //=======================================================
+        // process
+        //=======================================================
+
+        private static void onCreate( string assetPath ) {
+            var templateName = FileHelper.GetTemplateName( assetPath );
+
+            try {
+                AssetDatabase.CreateFolder( FileHelper.GetParentFolderName( assetPath ), templateName );
+                AssetDatabase.CreateFolder( assetPath, "Editor" );
+                generateScriptFromTemplate( templateName, assetPath );
+            } catch( Exception ex ) {
+                Debug.LogError( "TimelineScript: fail to save script file. :" + ex );
+            }
+
+            AssetDatabase.Refresh();
+        }
+
+        private static void generateScriptFromTemplate( string templateName, string assetPath ) {
+
+            for( int i = 0; i < FileHelper.TEMPLATE_FILENAME.Length; i++ ) {
+                var scriptTemplateName = FileHelper.TEMPLATE_FILENAME[ i ];
+                var templateText       = FileHelper.GetTemplateFile( scriptTemplateName );
+
+                var script     = setUpTemplate( templateName, templateText );
+                var scriptName = templateName + scriptTemplateName;
+                if( scriptTemplateName.Equals( "Drawer" ) )
+                    scriptName = "Editor/" + scriptName;
+
+                FileHelper.SaveScript( assetPath, script, scriptName );
+            }
+
+        }
+
+        private readonly static string REPLACE_STRING = "#CLASS#";
+        private static string setUpTemplate( string templateName, string text ) {
+            return text.Replace( REPLACE_STRING, templateName );
+        }
+
+    }
+
+}
---
+++ Assets/Plugins/TimelineScriptTemplate/Editor/Script/TimelineScriptTemplateFileHelper.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace charcolle.Utility.TimelineScriptTemplate {
+
+    internal static class FileHelper {
+
+        private readonly static string SEARCH_ROOT           = "TimelineScriptTemplateCreator";
+        private readonly static string RELATIVEPATH_TEMPLATE = "Editor/Template/";
+
+        internal readonly static string[] TEMPLATE_FILENAME = new string[] { "Clip",
+                                                                            "Behaviour",
+                                                                            "MixerBehaviour",
+                                                                            "Track",
+                                                                            "Drawer" };
+
+        //=======================================================
+        // path
+        //=======================================================
+
+        internal static string ScriptTemplatePath {
+            get {
+                return pathSlashFix( Path.Combine( TimelineScriptRootPath, RELATIVEPATH_TEMPLATE ) );
+            }
+        }
+
+        internal static string TimelineScriptRootPath {
+            get {
+                var guid = getAssetGUID( SEARCH_ROOT );
+
+                if( string.IsNullOrEmpty( guid ) ) {
+                    Debug.LogError( "fatal error." );
+                    return null;
+                }
+                var scriptPath = Path.GetDirectoryName( AssetDatabase.GUIDToAssetPath( guid ) );
+                var editorPath = Path.GetDirectoryName( scriptPath );
+                var rootPath   = Path.GetDirectoryName( editorPath );
+
+                return pathSlashFix( rootPath );
+            }
+        }
+
+        //=======================================================
+        // public
+        //=======================================================
+
+        internal static string GetParentFolderName( string assetPath ) {
+            return Path.GetDirectoryName( assetPath );
+        }
+
+        internal static string GetTemplateName( string assetPath ) {
+            return Path.GetFileNameWithoutExtension( assetPath );
+        }
+
+        internal static string GetSelectAssetPath() {
+            if( Selection.assetGUIDs == null || Selection.assetGUIDs.Length == 0 )
+                return Application.dataPath;
+
+            return AssetDatabase.GUIDToAssetPath( Selection.assetGUIDs[ 0 ] );
+        }
+
+        internal static string GetTemplateFile( string templateName ) {
+            var templatePath = AssetPathToSystemPath( Path.Combine( ScriptTemplatePath, templateName + ".template" ) );
+
+            try {
+                StreamReader sr  = new StreamReader( templatePath, Encoding.UTF8 );
+                var templateText = sr.ReadToEnd();
+                sr.Close();
+                return templateText;
+            } catch( Exception ex ) {
+                Debug.Log( "TimelineScript: fail to load template file. :" + ex );
+            }
+
+            return null;
+        }
+
+        internal static void SaveScript( string savePath, string script, string scriptName ) {
+            var scriptSavePath = pathSlashFix( Path.Combine( savePath, scriptName + ".cs" ) );
+
+            try {
+                StreamWriter sw = new StreamWriter( scriptSavePath, false, Encoding.UTF8 );
+                sw.Write( script );
+                sw.Close();
+            }
+            catch( Exception ex ) {
+                Debug.Log( "TimelineScript: fail to save script file. :" + ex );
+            }
+        }
+
+        internal static string AssetPathToSystemPath( string path ) {
+            return pathSlashFix( Path.Combine( dataPathWithoutAssets, path ) );
+        }
+
+        internal static string SystemPathToAssetPath( string path ) {
+            return pathSlashFix( path ).Replace( Application.dataPath, "Assets" );
+        }
+
+
+        //=======================================================
+        // private
+        //=======================================================
+
+        private static string getAssetGUID( string searchFilter ) {
+            var guids = AssetDatabase.FindAssets( searchFilter );
+            if( guids == null || guids.Length == 0 ) {
+                return null;
+            }
+
+            if( guids.Length > 1 ) {
+                Debug.LogWarning( "more than one file was found." );
+            }
+            return guids[ 0 ];
+        }
+
+        private const string forwardSlash = "/";
+        private const string backSlash = "\\";
+        private static string pathSlashFix( string path ) {
+            return path.Replace( backSlash, forwardSlash );
+        }
+
+        private static string dataPathWithoutAssets {
+            get {
+                return pathSlashFix( Application.dataPath.Replace( "Assets", "" ) );
+            }
+        }
+
+    }
+
+}
